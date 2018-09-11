c9b00c61d82610b4e12d233632afb49826270b2e
mulondo <mulondo26@gmail.com>
2018-09-11 20:30:14 +0300

    create make order method [#160394047]

--- api/app/models.py
+++ api/app/models.py
@@ -1,3 +1,16 @@
 class Customer_Orders:
     def __init__(self):
         self.orders=[]
+
+     def make_order(self,username,phone_number,myitems=list()):
+        my_order=[]
+        my_order=myitems
+        order_id=len(self.orders)+1
+        order={
+            "username":username,
+            "order_id":order_id,
+            "phone_number":phone_number,
+            "order_items":my_order,
+            "status":"None"
+        }
+        return self.orders.append(order)
