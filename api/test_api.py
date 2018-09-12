49075311b9d685bf63a56247ce9e2021367a34e0
mulondo <mulondo26@gmail.com>
2018-09-13 00:21:57 +0300

    Add test for get all orders[#160394115]

--- api/test_api.py
+++ api/test_api.py
@@ -12,6 +12,33 @@ class Test_api(TestCase):
     def test_add_order(self):
         result = self.client().post('/api/v1/orders', content_type='application/json',
         data=json.dumps(
-            dict(username="moses",phone_number="0703978578",order_items="['matooke']")))
-
+            dict(username="moses",phone_number="0704893645",order_items="['matooke']")))
         self.assertEqual(result.status_code, 201)
+
+    def test_short_phone_number(self):
+        result = self.client().post('/api/v1/orders', content_type='application/json',
+        data=json.dumps(
+            dict(username="moses",phone_number="07039",order_items="['matooke']")))
+        self.assertEqual(result.status_code, 403)
+
+    def test_very_long_phone_number(self):
+        result = self.client().post('/api/v1/orders', content_type='application/json',
+        data=json.dumps(
+            dict(username="moses",phone_number="07039573748483",order_items="['matooke']")))
+        self.assertEqual(result.status_code, 403)        
+    
+    def test_incorrect_phone_number_format(self):
+        result = self.client().post('/api/v1/orders', content_type='application/json',
+        data=json.dumps(
+            dict(username="moses",phone_number="fvhjccjaksnxkjk",order_items="['matooke']")))
+        self.assertEqual(result.status_code, 403)
+    
+    def test_empty_phone_number_format(self):
+        result = self.client().post('/api/v1/orders', content_type='application/json',
+        data=json.dumps(
+            dict(username="moses",phone_number="",order_items="['matooke']")))
+        self.assertEqual(result.status_code, 403)
+    def test_get_all_orders(self):
+        result = self.client().get('/api/v1/orders')
+        self.assertEqual(result.status_code, 200)
+    
