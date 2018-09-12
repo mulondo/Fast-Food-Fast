cd7384262d264eb0d962e61ea705520ef9935d84
mulondo <mulondo26@gmail.com>
2018-09-13 00:44:22 +0300

    Add get a specific order route [#160394172]

--- api/app/views.py
+++ api/app/views.py
@@ -11,10 +11,10 @@ def add_order():
     my_items.append(request.json['order_items'])
 
     # order items validation check
-    if len(my_items)<4:
-       return jsonify({'error':'No item is ordered'}),403
+    if len(my_items)==0:
+        return jsonify({'error':'No item is ordered'}),403
 
-    # user name validation check
+    # # user name validation check
     if username.strip()=="":
         return jsonify({'error':'customer name is empty'}),403    
     elif not username.isalpha():
@@ -27,19 +27,20 @@ def add_order():
     if phone_number.strip()=="":
         return jsonify({'error': 'phone number is empty'}),403
     elif not phone_number.isdigit():
-       return jsonify({'error': 'wrong phone number type'}),403
+        return jsonify({'error': 'wrong phone number type'}),403
     elif len(phone_number)<10:
         return jsonify({'error': 'phone number is too short'}),403
     elif len(phone_number)>12:
-        return jsonify({'error': 'phone number is too long'}),403
-   
-
-    
+        return jsonify({'error': 'phone number is too long'}),403    
     
     ordrs.make_order(username,phone_number,my_items)
     return jsonify({'message': 'succussfully created'}),201
 
 @myapp.route('/api/v1/orders',methods=['GET'])
 def get_order():
-    return jsonify({'orders made':ordrs.get_all_orders()})
+    return jsonify({'orders made':ordrs.get_all_orders()}),200
+
+@myapp.route('/api/v1/orders/<int:order_id>',methods=['GET'])
+def get_specific_order(order_id):
+    return jsonify({'Order':ordrs.get_an_order(order_id)}),200
    
