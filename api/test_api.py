95e0cd05ab1c60d313a1ea8db810f30c9c2f758e
mulondo <mulondo26@gmail.com>
2018-09-18 06:10:10 +0300

    remove duplicates

--- api/test_api.py
+++ api/test_api.py
@@ -27,13 +27,13 @@ class Test_api(TestCase):
     def test_very_long_phone_number(self):
         """ test for very long phone number"""
         result = self.client().post('/api/v1/orders', content_type='application/json',
-                                    data=json.dumps(dict(username="joan", phone_number="07039573748483", order_items="['matooke']")))
+                                    data=json.dumps(dict(username="joan", phone_number="07039500748483", order_items="['matooke']")))
         self.assertEqual(result.status_code, 403)
 
     def test_incorrect_phone_number_format(self):
         """ test for incorrect phone number format """
         result = self.client().post('/api/v1/orders', content_type='application/json',
-                                    data=json.dumps(dict(username="moses", phone_number="fvhjccjaksnxkjk", order_items="['matooke']")))
+                                    data=json.dumps(dict(username="sarah", phone_number="fvhjccjaksnxkjk", order_items="['matooke']")))
         self.assertEqual(result.status_code, 403)
 
     def test_empty_phone_number_format(self):
@@ -64,7 +64,7 @@ class Test_api(TestCase):
     def test_update_status_with_existing_id(self):
         """ test for update status """
         self.client().post('/api/v1/orders', content_type='application/json',
-                           data=json.dumps(dict(username="moses", phone_number="0705789625", order_items="['matooke']")))
+                           data=json.dumps(dict(username="Noah", phone_number="0705700834", order_items="['matooke']")))
 
         result = self.client().put('/api/v1/orders/1', content_type='application/json',
                                    data=json.dumps(dict(status="accepted")))
@@ -73,7 +73,7 @@ class Test_api(TestCase):
     def test_update_status_with_non_existing_id(self):
         """ test for update status """
         self.client().post('/api/v1/orders', content_type='application/json',
-                           data=json.dumps(dict(username="moses", phone_number="0705789625", order_items="['matooke']")))
+                           data=json.dumps(dict(username="Brian", phone_number="0705789625", order_items="['matooke']")))
 
         result = self.client().put('/api/v1/orders/10', content_type='application/json',
                                    data=json.dumps(dict(status="accepted")))
@@ -82,7 +82,7 @@ class Test_api(TestCase):
     def test_update_status_with_empty_status(self):
         """ test for empty status """
         self.client().post('/api/v1/orders', content_type='application/json',
-                           data=json.dumps(dict(username="moses", phone_number="", order_items="['chapati']")))
+                           data=json.dumps(dict(username="james", phone_number="", order_items="['chapati']")))
 
         result = self.client().put('/api/v1/orders/1', content_type='application/json',
                                    data=json.dumps(dict(status="")))
