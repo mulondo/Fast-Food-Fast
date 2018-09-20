e56f2e6db981ed808178139e35afbe30108f1940
mulondo <mulondo26@gmail.com>
2018-09-20 10:05:57 +0300

    move to outside

---
+++ app/views.py
@@ -0,0 +1,32 @@
+""" api routes"""
+from flask import jsonify, request
+from app.models import CustomerOrders
+from instance import myapp
+
+
+ORDRS = CustomerOrders()
+@myapp.route('/api/v1/orders', methods=['POST'])
+def place_order():
+    """creates an order"""
+    username = request.json['username']
+    phone_number = request.json['phone_number']
+    my_items = []
+    my_items.append(request.json['order_items'])
+    return ORDRS.make_order(username, phone_number, my_items)
+
+@myapp.route('/api/v1/orders', methods=['GET'])
+def get_order():
+    """Gets all orders"""
+    return ORDRS.get_all_orders()
+
+@myapp.route('/api/v1/orders/<int:order_id>', methods=['GET'])
+def get_specific_order(order_id):
+    """ gets a specific order given an id"""
+    return ORDRS.get_an_order(order_id)
+
+@myapp.route('/api/v1/orders/<int:order_id>', methods=['PUT'])
+def update_status(order_id):
+    """ Updates the status"""
+    status = request.json['status']
+    return ORDRS.update_status(order_id, status)
+    
