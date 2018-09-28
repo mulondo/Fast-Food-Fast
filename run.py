ed9bf4070611979d71f0aaad09d0008758d1e505
mulondo <mulondo26@gmail.com>
2018-09-28 22:20:44 +0300

    modify

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 """ runs the whole application """
-from api.instance import myapp
-from api.main import views
+from instance import myapp
+from main import views
 
 if __name__==('__main__'):
     myapp.run(debug=True, port=5000)
