0a4b877bef10695c800a7c1e70cfffcada982b18
mulondo <mulondo26@gmail.com>
2018-10-02 12:20:36 +0300

    add signup method

--- test/base_test.py
+++ test/base_test.py
@@ -2,4 +2,37 @@
 from unittest import TestCase
 from flask import json
 from main import views
+from unittest import TestCase
+from main.db import Database
 from instance import myapp
+
+
+class BaseTests(TestCase):
+    """create a base test class"""
+    def setUp(self):
+      obj=Database()      
+      creat_tab=obj.create_tables()
+      self.apps = myapp
+      self.client = self.apps.test_client
+      self.default_url='/api/v1/'
+
+    def signin_user(self,customer):
+      """ method registers user """
+      return self.client().post(self.default_url + "auth/signup", data=json.dumps(customer),
+                content_type='application/json')
+
+    def login(self):
+      """
+      method allows a user to login and get an access token
+      """
+      res = self.client.post(self.default_url+'/auth/login',
+                        content_type='application/json', data=json.dumps((login_user)))
+      result = json.loads(res.data.decode())
+      return result['access_token']
+    
+    def tearDown(self):
+        """
+        method destroys trial database after testing is done
+        """
+        trial_db = Database()
+        trial_db.drop_tables()
