6bf2083560a082b448eae4427deebefd5353f0c4
mulondo <mulondo26@gmail.com>
2018-10-02 15:32:42 +0300

    make order logic [#160903697]

--- main/models.py
+++ main/models.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify,json
 from main.db import Database
 import psycopg2
 
@@ -11,50 +11,17 @@ class CustomerOrders:
         """ contains instance variables"""
         self.orders = []
 
-    def make_order(self, username, phone_number, myitems=list()):
+    def make_order(self, customer_id, order_date, payment, current_location, my_items=list()):
         """ performs the logic for addding an order to a list"""
-        my_order = []
-        my_order = myitems
-        order_id = len(self.orders)+1
-
-        if not username.isalpha() or len(username) < 4 or username.strip() == "":
-            return jsonify({'error':'wrong username format'}), 403 
-
-        if not phone_number.isdigit() or phone_number.strip() == "" or len(phone_number) < 10 or len(phone_number) > 12:
-            return jsonify({'error': 'wrong phone number format'}), 403
-
-        order = {
-            "username":username,
-            "order_id":order_id,
-            "phone_number":phone_number,
-            "order_items":my_order,
-            "status":"None"
-        }
-        self.orders.append(order)
-        return jsonify({'message':'successfully added'}),201 
-
-    def get_all_orders(self):
-        """ gets a list of orders"""
-        return jsonify({'orders':self.orders}),200 
-
-    def get_an_order(self, order_id):
-        """ gets a specific order"""
-        for order in self.orders:
-            if order['order_id'] == order_id:
-                return jsonify({'order':order}),200
-        return jsonify({'error':'order doesnot exit'}),404
-
-    def update_status(self, order_id, status):
-        """ updates the order status"""
-        if status.strip() == "":
-            return jsonify({'error':'status is empty'}), 403
-        if not status.isalpha():
-            return jsonify({'error':'wrong stutus format'}), 403
-        for order in self.orders:
-            if order['order_id'] == order_id:
-                order['status'] = status
-                return jsonify({'status updated':self.orders}),201
-        return jsonify({'error':'The order id doesnot exist'}),404
+        customer_order = []
+        order_items = my_items
+        
+        try:
+            sql="INSERT INTO orders(customer_id,date,payment_mode,order_items,location) VALUES(%s,%s,%s,%s,%s)"
+            db_content.cur.execute(sql,(customer_id, order_date, payment, json.dumps(order_items),current_location))
+        except psycopg2.Error as err:
+            return jsonify({'error':str(err)}),400       
+        return jsonify({'message':'order succussfully made '}),201                                                        
 
     def create_account(self,username,phone,email,password):
         try:
