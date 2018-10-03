fc78990425e4d50d4cd9ec790b43c24846542dff
mulondo <mulondo26@gmail.com>
2018-10-03 08:39:52 +0300

    make tests [#160903697]

---
+++ test/test_order.py
@@ -0,0 +1,12 @@
+from test.base_test import BaseTests
+
+
+class Test_oders(BaseTests):
+
+    def test_make_order(self):        
+        """method tests for placing and order"""
+
+        order={"location":"kampala","payment_mode":"cash","order_items":[{"item":"chapati","price":"1000"}]}
+        result = self.client().post('/api/v1/users/orders',headers={'Authorization': 'Bearer '+ self.login(self.login_add)},
+                                                                                    json=dict(self.order))
+        self.assertEqual(result.status_code, 201)
