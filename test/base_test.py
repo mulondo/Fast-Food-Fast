cb322136915d5b29a92fb146d99eef3e7cbfec62
mulondo <mulondo26@gmail.com>
2018-10-04 06:02:38 +0300

    modify

--- test/base_test.py
+++ test/base_test.py
@@ -10,32 +10,43 @@ from instance import myapp
 class BaseTests(TestCase):
     """create a base test class"""
     def setUp(self):
-      obj=Database()      
-      creat_tab=obj.create_tables()
       self.apps = myapp
       self.client = self.apps.test_client
-      self.default_url='/api/v1/'
+      self.order={'location':'kampala',
+                'payment_mode':'cash',
+                'order_items':[{'items':'eggs','price':'1000'}]}
+      self.customer={'phone_number':'0703455445',
+                      'email':'opi@gmail.com',
+                      'username':'opio',
+                      'password':'opioqwt'
+                  }
+      self.login_add={'username':'opio','password':'opioqwt'}
 
-    def signin_user(self,customer):
-      """ method registers user """
-      return self.client().post(self.default_url + "auth/signup", data=json.dumps(customer),
-                content_type='application/json')
-    
-    def plan_login(self,login_info):
-      """ method for plan login """
-      return self.client().post(self.default_url+'auth/login',
-                        content_type='application/json', data=json.dumps((login_info)))
+    def test_signup(self):
+        """ test for posting an order """
+        add_result = self.client().post('/api/v1/auth/signup', content_type='application/json', data=json.dumps(self.customer))
+        self.assertEqual(add_result.status_code, 201)
 
-    def login(self,login_info):
-      """ method allows a user to login and get an access token """
-      res = self.client().post(self.default_url+'auth/login',
-                        content_type='application/json', data=json.dumps((login_info)))
-      result = json.loads(res.data.decode())
-      return result['access_token']
+    def login(self,tp):
+        respo=add_result=self.client().post('/api/v1/auth/login',content_type='application/json', data=json.dumps(self.login_add))
+        respo_data=json.loads(respo.data.decode())
+        token=respo_data['access_token']
+        return token
     
-    def tearDown(self):
-        """
-        method destroys trial database after testing is done
-        """
-        trial_db = Database()
-        trial_db.drop_tables()
+    # def plan_login(self,login_info):
+    #   """ method for plan login """
+    #   return self.client().post(self.default_url+'auth/login',
+    #                     content_type='application/json', data=json.dumps(login_info))
+    
+    # def make_user_admin(self):
+    #   return self.client().put('/make_admin/<int:user_id>',content_type='application/json',data=json.dumps(user_id))
+                                                                              
+    
+    # def make_order(self,order,token):
+    #   return self.client().post(self.default_url+'users/orders',content_type='application/json',
+    #                       data=json.dumps(order),headers=({"acces_token": token}))                       
+    
+    # def tearDown(self):
+    #     """ method destroys trial database after testing is done """
+    #     trial_db = Database()
+    #     trial_db.drop_tables()
