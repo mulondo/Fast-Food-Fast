4990e2005f1d80d6b5159c01c856752ef923053d
mulondo <mulondo26@gmail.com>
2018-10-04 04:38:35 +0300

    update

--- test/test_user.py
+++ test/test_user.py
@@ -6,7 +6,7 @@ class Test_user(BaseTests):
     def test_user_signup(self):
         """method testing for sign up"""      
         reg=self.signin_user(self.customer)
-        self.assertEqual(reg.status_code,201)
+        self.assertEqual(reg.status_code,200)
     def test_login(self):
         res=self.login(self.login_add)
         self.assertEqual(res.status_code,201)
