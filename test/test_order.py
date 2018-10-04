c7ace4ca638868e6402062a643000b1f69fba94b
mulondo <mulondo26@gmail.com>
2018-10-04 04:25:13 +0300

    test for login

--- test/test_order.py
+++ test/test_order.py
@@ -7,6 +7,6 @@ class Test_oders(BaseTests):
         """method tests for placing and order"""
 
         order={"location":"kampala","payment_mode":"cash","order_items":[{"item":"chapati","price":"1000"}]}
-        result = self.client().post('/api/v1/users/orders',headers={'Authorization': 'Bearer '+ self.login(self.login_add)},
+        result = self.client().post('/api/v1/users/orders',headers={'Authorization': 'Bearer '+ self.tok_login(self.login_add)},
                                                                                     json=dict(self.order))
         self.assertEqual(result.status_code, 201)
