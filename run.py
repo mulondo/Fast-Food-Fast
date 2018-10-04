f4c42eeb187b7f412480511c4033b7130a01d313
mulondo <mulondo26@gmail.com>
2018-10-04 03:59:12 +0300

    modify

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 """ runs the whole application """
 from instance import myapp
+from main.db import Database
 from main import views
-
+db=Database()
 if __name__==('__main__'):
     myapp.run(debug=True, port=5000)
+    db.create_tables()
