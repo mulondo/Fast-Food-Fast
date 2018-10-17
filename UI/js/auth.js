8f9f64c8d3bf8a1ccf50d7280a85fec04a534e8f
mulondo <mulondo26@gmail.com>
2018-10-17 08:42:41 +0300

    edit

--- UI/js/auth.js
+++ UI/js/auth.js
@@ -1,10 +1,10 @@
-document.getElementById("signup").addEventListener("click",onclick);
-function onclick(){
+document.getElementById("signup").addEventListener("click",onclick_signup);
+function onclick_signup(){
     var data={email:document.getElementById('mail').value,
     username:document.getElementById('usrn').value,
     password:document.getElementById('pwd').value,
     phone_number:document.getElementById('phno').value
-    }
+    };
     // const url ='http://127.0.0.1:5000/api/v2/auth/signup';
     fetch('http://127.0.0.1:5000/api/v2/auth/signup',{
     method: 'POST',
@@ -26,29 +26,3 @@ function onclick(){
     .catch(error=>alert("errors"));
   
 }
-
-
-
-function on_click_login() {
-    var passwd = document.getElementById('pwd');
-    var usern  = document.getElementById('usrn');
-    var email_d = document.getElementById('mail');
-    var phone = document.getElementById('phno');
-
-     if (!usern.checkValidity()|| !passwd.checkValidity()|| !phone.checkValidity()){
-         alert("username or password is empty");
-         return false;
-         }
-    else if(!phone.checkValidity()){
-            alert("phone number is empty or invalid");
-            return false;
-         }
-    else if(!email_d.checkValidity()){
-            alert("email is empty or invalid");
-            return false;
-    }
-     else{
-         window.location.href = "customer.html";                        
-     }
-     document.getElementById('display').innerHTML = value;
- }
