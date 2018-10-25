0a73f255599a1a17f9a5e59b5c55539e4cabdaad
mulondo <mulondo26@gmail.com>
2018-10-25 09:56:59 +0300

    edit

--- UI/js/makeorder.js
+++ UI/js/makeorder.js
@@ -1,16 +1,11 @@
 // pick modals
 var breakfastmodal = document.getElementById('breakfastmodal');
-var lunchmodal= document.getElementById('lunchmodal');
-var dinnermodal= document.getElementById('dinnermodal');
+
 
 
 // pick buttons
 var breakfast_btn = document.getElementById("breakfast_btns");
-var lunch_btn=document.getElementById("lunch_btn_id");
-var dinner_btn=document.getElementById("dinner_btn_id");
-var lunchcancel_btn=document.getElementById("lunchcancel");
 var breakcancel_btn=document.getElementById("breakcancel");
-var dinnercancel_btn=document.getElementById("dinnercancel");
 var dinner_order_btn=document.getElementById("dinner_order");
 var make_order_btn=document.getElementById("submit_breakfast")
 
@@ -33,35 +28,34 @@ breakfast_btn.onclick = function() {
     .then(response=>{
         data=response.results;
         var i;
+        var row = `<table id="breakfast">
+                    <tr>                             
+                    <th>Item</th>                                    
+                    <th>Quantity</th>
+                    <th>Price</th>                   
+                    </tr>`;
         for (i=0;i<data.length;i++){             
             if(data[i].category=="breakfast"){
-                var option=document.createElement("option");
-                var checkbox=document.createElement("input");
-                checkbox.setAttribute('type','textarea');   
-                var newrow=breakfast_table.insertRow(1);
-                var item=newrow.insertCell(0);                
-                var quantity=newrow.insertCell(1);
-                var price=newrow.insertCell(2);
-                var check=newrow.insertCell(3);
-                check=checkbox;
-                item.innerHTML=data[i].item_name;                
-                price.innerHTML=data[i].price;
-                quantity.innerHTML=data[i].quantity;
+                var option=document.createElement("option");              
                 option.textContent=data[i].item_name;
                 option.value=data[i].item_name;
                 selected.appendChild(option);
+                row += '<tr><td>'+data[i].item_name+'</td><td>'+data[i].price+'</td><td>'+data[i].quantity+'</td></tr>';
+                // row += '<tr><td>'+data[i].item_name+'</td><td>'+data[i].price+'</td><td>'+data[i].quantity+'</td><td><button onclick="getId()">order_id</button></td></tr>';
             }
             else{
                 console.log("well");
-            }          
+            } 
+            document.getElementById("table").innerHTML = row+"</table>";         
         }
       
     })
     .catch(error=>alert("errors"));
 }
 
+
 make_order_btn.onclick=function(){   
-   items_data={
+    items_data={
     location:document.getElementById("location").value,
     payment_mode:document.getElementById("payment").value,        
     order_items:document.getElementById("selected").value
@@ -82,20 +76,14 @@ fetch('https://real-fast-food-fast.herokuapp.com/api/v2/users/orders',{
 
 location.reload();
 
-    alert("order made succussfully");
+alert("order made succussfully");
 }
-
 
 
 breakcancel_btn.onclick = function() {
     breakfastmodal.style.display = "none";
 }
-lunch_btn.onclick =function() {
-    lunchmodal.style.display = "block";
-}
-lunchcancel.onclick =function() {
-    lunchmodal.style.display = "none";
-}
+
 dinner_btn.onclick =function() {
     dinnermodal.style.display = "block";
 }
